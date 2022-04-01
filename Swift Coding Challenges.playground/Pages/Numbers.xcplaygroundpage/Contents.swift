ff48616fc128bf9144a6e7ecf5da4ca3851fda0a
nadiasiddiqah <nadiasiddiqah@gmail.com>
2022-03-31 17:35:38 -0700

    Completed Numbers coding challenges

--- Swift Coding Challenges.playground/Pages/Numbers.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Numbers.xcplaygroundpage/Contents.swift
@@ -1,7 +1,68 @@
-//: [Previous](@previous)
-
 import Foundation
+import Darwin
 
-var greeting = "Hello, playground"
+// MARK: - CHALLENGE 16
 
-//: [Next](@next)
+//challenge16()
+
+// MARK: - CHALLENGE 17
+
+//challenge17(min: 1, max: 5)
+//challenge17(min: 8, max: 10)
+//challenge17(min: 12, max: 12)
+//challenge17(min: 12, max: 18)
+
+// MARK: - CHALLENGE 18
+
+//challenge18(input: 4, power: 3)
+//challenge18(input: 2, power: 8)
+//challenge18(input: 0, power: 3)
+
+// MARK: - CHALLENGE 19
+
+// MARK: - CHALLENGE 20
+
+//challenge20(input: 2)
+//challenge20(input: 11)
+//challenge20(input: 13)
+//challenge20(input: 4)
+//challenge20(input: 9)
+//challenge20(input: 16777259)
+
+// MARK: - CHALLENGE 21
+
+//challenge21(number: 12)
+//challenge21(number: 28)
+
+// MARK: - CHALLENGE 22
+
+//challenge22(number: 32)
+//challenge22(number: 41)
+//challenge22(number: 4)
+//challenge22(number: 148)
+
+// MARK: - CHALLENGE 23
+
+//challenge23(input: "01010101")
+//challenge23(input: "123456789")
+//challenge23(input: "9223372036854775808")
+//challenge23(input: "1.01")
+
+// MARK: - CHALLENGE 24
+
+//challenge24(input: "a1b2c3")
+//challenge24(input: "a10b20c30")
+//challenge24(input: "h8ers")
+
+// MARK: - CHALLENGE 25
+
+//challenge25(number: 1)
+//challenge25(number: 9)
+//challenge25(number: 16777216)
+//challenge25(number: 16)
+//challenge25(number: 15)
+
+// MARK: - CHALLENGE 26
+
+//challenge26(subtract: 5, from: 9)
+//challenge26(subtract: 10, from: 30)
